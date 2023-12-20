74172f6b354f05f4f33c0cb106a9f224de0aa0d4
Nevermind <nurbekuuludaniel@gmail.com>
2023-12-21 00:47:19 +0600

    add jinja 'for' cycle

--- products/models.py
+++ products/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 
 class Mice(models.Model):
-    tittle = models.CharField('Название', max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(max_length=300)
+    tittle = models.CharField( max_length=50, verbose_name="Заголовок")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    description = models.TextField(max_length=300,verbose_name="Описание")
+    image = models.ImageField(upload_to='images/', blank=False, verbose_name="Изображение")
 
     def __str__(self):
         return self.tittle
